1a87239b305918f95c3a83dd92bccbf1f31cb790
ikocis1999 <imra.kocis@vuv.hr>
2021-04-28 19:41:45 +0200

    todolist

--- js/todolist.js
+++ js/todolist.js
@@ -1,3 +1,15 @@
+var pero = '"far fa-trash-alt"';
+var counter = 0;	
+		
+function Obrisi(x) {
+	$('#list #'+x).remove();
+}
 
+function Dodaj(){
+    counter++;
+    var ul = $('#list');
+    var zadatak = $('#input').val();
+    ul.append(`<li id="${counter}"> <input type="checkbox"> ${zadatak} <button onclick="Obrisi(${counter})"><i class="fas fa-trash"></i></button></li>`);
+}
 
 
